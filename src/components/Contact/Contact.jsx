6e8675d47a5bce8a237d3ae61d70a96a85b3c203
berl weber <153825363+berlweber@users.noreply.github.com>
2026-08-28 13:37:37 +0200

    styled contact section at desktop width

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,7 @@
 import './Contact.css';
 import { Mail } from 'lucide-react';
 import { FaGithub } from "react-icons/fa6";
-import { FaLinkedin } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
 import { FiPhone } from "react-icons/fi";
 import { BsSend } from "react-icons/bs";
 import { useState } from 'react';
@@ -31,80 +31,99 @@ const Contact = () => {
 
     return (
         <section id='contact' className='contact'>
-          <h2>Contact</h2>
-            <p>
-            I’m currently looking for junior full-stack or back-end focused developer
-            opportunities, remote or local. I’m interested in building reliable,
-            practical software with thoughtful teams that value clear communication,
-            quality work, and real-world problem solving.
-            </p>
-            <ul className='contact-details'>
-                <li>
-                    <Mail size={20} />
-                    <a href="mailto:a5142732357@gmail.com">a5142732357@gmail.com</a>
-                </li>
-                <li>
-                    <FiPhone size={20} />
-                    <a href="tel:+32488305152">+32488 30 51 52</a>
-                </li>
-                <li>
-                    <FaLinkedin size={20} />
-                    <a href="https://www.linkedin.com/in/berl-weber-341165408/" target='_blank' rel='noreferrer'>linkedin.com/in/berl-weber</a>
-                </li>
-                <li>
-                    <FaGithub size={20} />
-                    <a href="https://github.com/berlweber" target='_blank' rel='noreferrer'>github.com/berlweber</a>
-                </li>
-            </ul>
-            <form onSubmit={handleSubmit}> {/*action="mailto:a5142732357@gmail.com" method='post' encType='text/plain'*/}
-                <label htmlFor="name">Name</label>
-                 <input 
-                    type="text" 
-                    name='name'
-                    id='name'
-                    placeholder='Your Name'
-                    required
-                    value={formData.name} 
-                    onChange={handleChange}
-                />
-                <label htmlFor="email">Email</label>
-                 <input 
-                    type="email" 
-                    name='email'
-                    id='email'
-                    placeholder='example@myself.dev'
-                    required
-                    value={formData.email} 
-                    onChange={handleChange}
-                />
+            <div className='contact-content'>
+                <h2>Contact</h2>
+                <p>I’m currently looking for junior full-stack or back-end-focused
+                     opportunities, remotely or in the Antwerp area. If you’re building
+                      practical software and think I could contribute to your team, I’d
+                       be glad to hear from you.</p>
+                <ul className='contact-details'>
+                    <li>
+                        <a href="mailto:a5142732357@gmail.com">
+                            <Mail size={24} />
+                            a5142732357@gmail.com
+                        </a>
+                    </li>
+                    <li>
+                        <a href="tel:+32488305152">
+                            <FiPhone size={24} />
+                            +324 88 30 51 52
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.linkedin.com/in/berl-weber-341165408/" target='_blank' rel='noreferrer'>
+                            <FaLinkedinIn size={24} />
+                            linkedin.com/in/berl-weber
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/berlweber" target='_blank' rel='noreferrer'>
+                            <FaGithub size={24} className='github-symbol'/>
+                            github.com/berlweber
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <form onSubmit={handleSubmit} className='contact-form'> {/*action="mailto:a5142732357@gmail.com" method='post' encType='text/plain'*/}
+                <div className='name-and-email'>
+                    <div className='input-wrapper'>
+                        <label htmlFor="name">Name</label>
+                        <input
+                            type="text"
+                            name='name'
+                            id='name'
+                            placeholder='Jane Smith'
+                            required
+                            className='name'
+                            value={formData.name}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className='input-wrapper'>
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            name='email'
+                            id='email'
+                            placeholder='jane.smith@example.com'
+                            required
+                            className='email'
+                            value={formData.email}
+                            onChange={handleChange}
+                        />
+                    </div>
+                </div>
                 <label htmlFor="subject">Subject</label>
-                 <input 
-                    type="text" 
-                    name='subject' 
+                <input
+                    type="text"
+                    name='subject'
                     id='subject'
-                    placeholder='Subject'
+                    placeholder='Junior developer opportunity'
                     required
-                    value={formData.subject} 
+                    className='subject'
+                    value={formData.subject}
                     onChange={handleChange}
                 />
                 <label htmlFor="message">Message</label>
-                 <textarea 
-                    name="message" 
-                    placeholder='Write your message here...'
+                <textarea
+                    name="message"
+                    placeholder='Hi Berl, I’d like to discuss an opportunity...'
                     id='message'
                     required
-                    value={formData.message} 
-                    cols={50} rows={3} 
+                    className='text-area'
+                    value={formData.message}
+                    cols={50} rows={3}
                     onChange={handleChange}
-                    ></textarea>
-                 <button 
-                 type='submit' 
+                    >
+                </textarea>
+                <button
+                 type='submit'
                  className='submit-btn'>
                     Send Message
                     <BsSend size={16}/>
                  </button>
                  {/* still need to make the submit button working */}
-            </form>   
+            </form>
         </section>
     );
 };
